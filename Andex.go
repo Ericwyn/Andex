c814ad3ede044ba899b4cc1c1f037976dfffdc57
Ericwyn <ericwyn.chen@gmail.com>
2021-03-03 20:06:48 +0800

    增加 corn 自动刷新 token

--- Andex.go
+++ Andex.go
@@ -7,6 +7,7 @@ import (
 	"github.com/Ericwyn/Andex/controller"
 	"github.com/Ericwyn/GoTools/file"
 	"github.com/gin-gonic/gin"
+	"github.com/go-co-op/gocron"
 	"net/http"
 	"os"
 	"time"
@@ -14,26 +15,28 @@ import (
 
 func main() {
 
-	//loadConfig()
-	conf.LoadConfFromFile()
-	//
-	info := api.UserInfo()
-	if info != nil {
-		fmt.Println("token 未过期")
-	} else {
-		fmt.Println("token 已过期")
-		loadConfig()
-	}
-	//fmt.Println("用户昵称:", info.NickName)
-
 	////// 获取文件夹文件列表
 	//list := api.FolderList("root")
 	//for _,item := range list.Items {
 	//	fmt.Println("name:", item.Name, ", type:", item.Type, ", id:", item.FileID)
 	//}
 
+	loadConfig()
+
+	runCorn()
+
 	startServer()
 
+}
+
+func runCorn() {
+	s := gocron.NewScheduler(time.UTC)
+
+	// 每 30 分钟刷新一次配置
+	s.Every(30).Minutes().Do(func() {
+		fmt.Println("corn 刷新 token 配置")
+		api.RefreshToken()
+	})
 }
 
 func loadConfig() {
@@ -43,6 +46,7 @@ func loadConfig() {
 		conf.SaveConf()
 		os.Exit(0)
 	}
+
 	// 载入配置
 	conf.LoadConfFromFile()
 	if conf.ConfigNow.RefreshToken == "NULL" {
@@ -50,13 +54,21 @@ func loadConfig() {
 		os.Exit(0)
 	}
 
-	// 刷新配置
-	api.RefreshToken()
-
 	if conf.ConfigNow.Authorization == "NULL" {
 		fmt.Println("RefreshToken 错误或已过期")
 		os.Exit(0)
 	}
+
+	// 载入配置后验证 token 是否已过期
+	info := api.UserInfo()
+	if info != nil {
+		fmt.Println("token 未过期")
+	} else {
+		fmt.Println("token 已过期, 刷新配置")
+		// 刷新配置
+		api.RefreshToken()
+	}
+
 }
 
 func startServer() {
--- go.mod
+++ go.mod
@@ -5,5 +5,5 @@ go 1.13
 require (
 	github.com/Ericwyn/GoTools v0.0.0-20210121122108-55fad04a172d
 	github.com/gin-gonic/gin v1.6.3
-	github.com/json-iterator/go v1.1.9
+	github.com/go-co-op/gocron v0.7.0
 )
